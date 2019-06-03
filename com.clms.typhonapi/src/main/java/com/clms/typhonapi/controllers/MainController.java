ba6fd337ddf7cb69cfa0f70aa61bf7ed59c97aee
Dimitris Moudilos <d.moudilos@clmsuk.com>
2019-06-03 13:10:47 +0300

    Enable CORS requests

--- com.clms.typhonapi/src/main/java/com/clms/typhonapi/controllers/MainController.java
+++ com.clms.typhonapi/src/main/java/com/clms/typhonapi/controllers/MainController.java
@@ -9,10 +9,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 public class MainController {
 
 
