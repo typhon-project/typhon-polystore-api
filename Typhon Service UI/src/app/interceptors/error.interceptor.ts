39502860bbe46840c0652676f567c2059726bfbe
Dimitris Moudilos <d.moudilos@clmsuk.com>
2019-06-21 09:32:40 +0300

    general error handling

--- Typhon Service UI/src/app/interceptors/error.interceptor.ts
+++ Typhon Service UI/src/app/interceptors/error.interceptor.ts
@@ -4,13 +4,18 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import { ApiService } from '../api.service';
+import { Ng6NotifyPopupService } from 'ng6-notify-popup';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: ApiService) {}
+    constructor(private authenticationService: ApiService, private notify: Ng6NotifyPopupService) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(catchError(err => {
+            if (err.status === 500) {
+                this.notify.show(err.error.message, { position:'bottom', duration:'3000', type: 'error' });
+            }
+
             if (err.status === 401) {
                 // auto logout if 401 response returned from api
                 this.authenticationService.logout();
