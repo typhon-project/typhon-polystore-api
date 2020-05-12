dfb0ff97dcb004c146d6c138251bbc47a972ca78
Orfen <o.panagou@gmail.com>
2020-05-12 16:06:30 +0300

    fix swagger.json path

--- com.clms.typhonapi/src/main/java/com/clms/typhonapi/config/SwaggerJsonConfiguration.java
+++ com.clms.typhonapi/src/main/java/com/clms/typhonapi/config/SwaggerJsonConfiguration.java
@@ -27,7 +27,7 @@ public class SwaggerJsonConfiguration {
 
     @Bean
     public SwaggerJson swaggerConfiguration(
-            @Value("src/main/resources/swagger.json") String location,
+            @Value("${project.build.outputDirectory}/swagger.json") String location,
             @Qualifier("webApplicationContext") ResourceLoader loader) throws IOException {
 
         InputStream istream = getResource(location, loader);
