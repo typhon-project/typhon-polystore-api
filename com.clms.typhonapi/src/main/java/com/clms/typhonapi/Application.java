1a147e01de046596d950cd4d7f4f7688768e4557
Orfen <o.panagou@gmail.com>
2020-05-12 18:01:39 +0300

    configure logging

--- com.clms.typhonapi/src/main/java/com/clms/typhonapi/Application.java
+++ com.clms.typhonapi/src/main/java/com/clms/typhonapi/Application.java
@@ -1,12 +1,16 @@
 package com.clms.typhonapi;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.logging.log4j.spi.LoggerContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class Application {
 	
     public static void main(String[] args) {
+        BasicConfigurator.configure();
     	SpringApplication.run(Application.class, args);
 
     }    
