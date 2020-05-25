320aeab8aa9afdafc8eb55b0958683cdb117c3b7
Orfen <o.panagou@gmail.com>
2020-05-25 21:07:10 +0300

    exclude gson autoconfig

--- com.clms.typhonapi/src/main/java/com/clms/typhonapi/Application.java
+++ com.clms.typhonapi/src/main/java/com/clms/typhonapi/Application.java
@@ -5,9 +5,10 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {GsonAutoConfiguration.class})
 public class Application {
 	
     public static void main(String[] args) {
